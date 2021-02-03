fd36f93bbdedf5585cc0bf119b830dbc6ed7d537
Marco von Ballmoos <marco@encodo.com>
2021-02-03 16:02:41 +0100

    Restore Appveyor tests to original

--- appveyor/index.spec.ts
+++ appveyor/index.spec.ts
@@ -1,35 +1,8 @@
-import { readFileSync, statSync } from 'fs-extra';
-import { glob } from 'glob';
+const snapshot = require('../snapshots/AdventureWorks2017.json');
+const cache = require('../_sql-database/cache.json');
 
 describe('appveyor', () => {
-  it('file contents should match snapshots', () => {
-    const getAllFilenames = (folder: string): string[] => {
-      return glob.sync(folder + '**/*');
-    };
-
-    const snapshots = getAllFilenames(
-      'snapshots/AdventureWorks2017/_sql-database/'
-    );
-    const results = getAllFilenames('_sql-database/');
-
-    // Just in case the glob results aren't in a predictable order
-    snapshots.sort();
-    results.sort();
-
-    expect(snapshots.length).toEqual(results.length);
-
-    snapshots.map((snapshot, index) => {
-      const result = results[index];
-
-      expect(snapshot).toContain(result);
-
-      const stat = statSync(result);
-      if (stat.isFile()) {
-        const resultContent = readFileSync(result).toString();
-        const snapshotContent = readFileSync(snapshot).toString();
-
-        expect(resultContent).toEqual(snapshotContent);
-      }
-    });
+  it('cache should match snapshot', () => {
+    expect(cache).toEqual(snapshot);
   });
 });
